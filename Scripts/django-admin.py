f72816c50c5001d0d17711b8bb7d05c5c7cb19bd
diegomjasso <maciasjasso0@hotmail.com>
2017-04-10 16:13:41 -0500

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\ge_data_classification\confidential\git\my_carwash\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
