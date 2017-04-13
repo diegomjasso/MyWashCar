8d8a18bf00d93ec87dc796ef0d4f1ae9b93aec50
diegomjasso <maciasjasso0@hotmail.com>
2017-04-13 09:24:57 -0500

    fix the static file

---
+++ my_carwash/static/assets/javascript/index.js
@@ -0,0 +1,13 @@
+(function(){
+	var extenxionJS = '.js';
+	var sourceScripts = '../../static/assets/javascript/';
+	var scriptsList =	[
+						];
+	for(var i = 0; i < scriptsList.length; i++){
+		var scriptTagHTML = document.createElement('script');
+		scriptTagHTML.type ='text/javascript';
+		scriptTagHTML.async = false;
+		scriptTagHTML.src = sourceScripts + scriptsList[i] + extenxionJS;
+		(document.getElementsByTagName('HEAD')[0]||document.body).appendChild(scriptTagHTML);
+	};
+})();
