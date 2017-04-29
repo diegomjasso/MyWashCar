a1a6fa285ff1d9324effccdc48bbe310428b6782
diegomjasso <maciasjasso0@hotmail.com>
2017-04-28 21:26:42 -0500

    diegoMacias

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\ge_data_classification\confidential\git\my_carwash\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
