746cb9cda0e9b92c2f6c03698f90c8f6423b9ba5
Diego Eduardo DEMJ. Macias Jasso <diego.macias@softtekge.com>
2017-05-19 15:45:25 -0500

    Landing Page

--- static/assets/javascript/index.js
+++ static/assets/javascript/index.js
@@ -2,7 +2,9 @@
 	var extenxionJS = '.js';
 	var sourceScripts = '../../static/assets/javascript/';
 	var scriptsList =	[
+							'app/styles'
 						];
+
 	for(var i = 0; i < scriptsList.length; i++){
 		var scriptTagHTML = document.createElement('script');
 		scriptTagHTML.type ='text/javascript';
@@ -10,4 +12,8 @@
 		scriptTagHTML.src = sourceScripts + scriptsList[i] + extenxionJS;
 		(document.getElementsByTagName('HEAD')[0]||document.body).appendChild(scriptTagHTML);
 	};
+
+	setTimeout( function()  {
+    	setStyles.init();
+  	},	400);
 })();
