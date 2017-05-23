9d23b35a8d2c0c61029e196ede81936889e40202
Diego Eduardo DEMJ. Macias Jasso <diego.macias@softtekge.com>
2017-05-23 17:11:36 -0500

    mao and markers

--- static/assets/javascript/app/dashboard.js
+++ static/assets/javascript/app/dashboard.js
@@ -33,19 +33,17 @@ var dashboard = {
       };
     },
     openSide: function() {
-      $("#map-content").addClass("map-content-closed");
       $("#side-dashboard-container").addClass("side-dashboard-container-opened");
       $("#side-dashboard").addClass("side-dashboard-opened");
       $("#direction-icnon-sidelash").addClass("fa-angle-double-left");
       
-      $("#map-content").removeClass("map-content-opened");
+      $("#map-content").removeClass("map-content-closed");
       $("#side-dashboard-container").removeClass("side-dashboard-container-closed");
       $("#side-dashboard").removeClass("side-dashboard-closed");
       $("#direction-icnon-sidelash").removeClass("fa-angle-double-right");
       dashboard.ui.sideStatus = true;
     },
     closeSide: function() {
-      $("#map-content").removeClass("map-content-closed");
       $("#side-dashboard-container").removeClass("side-dashboard-container-opened");
       $("#side-dashboard").removeClass("side-dashboard-opened");
       $("#direction-icnon-sidelash").removeClass("fa-angle-double-left");
@@ -58,34 +56,62 @@ var dashboard = {
     }
   },
   map: {
+    carWashPoints: [
+        ['Bondi Beach', -33.890542, 151.274856, 4],
+        ['Coogee Beach', -33.923036, 151.259052, 5],
+        ['Cronulla Beach', -34.028249, 151.157507, 3],
+        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+        ['Maroubra Beach', -33.950198, 151.259302, 1]
+      ],
     showMap: function ()  {
       mapObject = document.getElementById('map');
       if (  mapObject != null) {
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function (p) {
             var LatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
-            /*var myLatLng = {lat: 21.9589651, lng: -102.290256};*/
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 13,
-              center: LatLng,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
-
-            var marker = new google.maps.Marker({
-              position: LatLng,
-              map: map,
-              title: "<div style = 'height:60px;width:200px'><b>Your location:</b><br />Latitude: " + p.coords.latitude + "<br />Longitude: " + p.coords.longitude
-            });
-
-            google.maps.event.addListener(marker, "click", function (e) {
-                var infoWindow = new google.maps.InfoWindow();
-                infoWindow.setContent(marker.title);
-                infoWindow.open(map, marker);
-            });
           });
         } else {
+          var LatLng = {lat: 21.9589651, lng: -102.290256};
           alert('Geo Location feature is not supported in this browser.');
         }
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+        /*  center: LatLng,*/
+          center: {lat: -33.9, lng: 151.2},
+        });
+
+        dashboard.map.setMarkers( map);
+      };
+    },
+    setMarkers:  function(  map) {
+      var image = {
+        // Set the icon for the markers on the map
+        /*url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',*/
+        url: '/static/assets/images/marker.png',
+        // This marker is 20 pixels wide by 32 pixels high.
+        size: new google.maps.Size(20, 32),
+        // The origin for this image is (0, 0).
+        origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        anchor: new google.maps.Point(0, 32)
+      };
+
+      var shape = {
+        coords: [1, 1, 1, 20, 18, 20, 18, 1],
+        type: 'poly'
+      };
+      
+      for (var i = 0; i < dashboard.map.carWashPoints.length; i++) {
+        var carWash = dashboard.map.carWashPoints[i];
+        var marker = new google.maps.Marker({
+          position: {lat: carWash[1], lng: carWash[2]},
+          map: map,
+          icon: image,
+          shape: shape,
+          title: carWash[0],
+          zIndex: carWash[3]
+        });
       }
     },
     fixMapHeiight: function ()  {
