4bf67bcee6faeeb99de8571832a67421d848d573
rogelio ortiz acevedo <rogelio.ortiz@softtek.com>
2017-05-23 10:03:44 -0500

    agregando la funcionalidad del registro de usuarios

--- my_carwash/views.py
+++ my_carwash/views.py
@@ -8,9 +8,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView, View
+from .forms import CreateUserForm
 
-def index(request):
-	return render(request, 'index.html', {})
+class IndexClass(CreateView):
+	success_url = reverse_lazy('dashboard')
+	model = User
+	template_name = 'index.html'
+	form_class =  CreateUserForm
+
+	def form_valid(self, form):
+		self.object = form.save(commit = False)
+		self.object.set_password(self.object.password)
+		self.object.save()
+
+		return HttpResponseRedirect(self.get_success_url())
 
 def error_404(request):
 	return render(request, 'error_404.html', {})
