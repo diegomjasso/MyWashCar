ddbd17458968259b73bf6fde288d55e433efe30b
rogelio ortiz acevedo <rogelio.ortiz@softtek.com>
2017-06-01 16:19:24 -0500

    filtrando queryset de los usuarios para mostrar solo info del user logueado

--- perfiles/viewsets.py
+++ perfiles/viewsets.py
@@ -5,3 +5,11 @@ from .serializers import UserSerializer
 class PerfilViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
+
+	def get_queryset(self):
+		if self.request.user.is_superuser:
+			return self.queryset.all()
+		else:
+			return self.queryset.filter(id = self.request.user.id)
+
+		
